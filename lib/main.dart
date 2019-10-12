9022e2511586aab4ac759fc619132a9028b7a384
iuri1911 <contato@iuri.tech>
2019-10-12 00:21:25 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+const request = "https://api.hgbrasil.com/finance/quotations?key=904fe73e";
+
+
+void main(){
+  runApp(MaterialApp(
+    home: Container(),
+  ));
+   
+}
